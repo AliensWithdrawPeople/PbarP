f2c7d3e1b7ba783703b98490140b950cc2083764
AliensWithdrawPeople <daniilivanov1606@gmail.com>
2023-09-14 23:06:41 +0700

    Preliminary selection script for coll PbaP.

---
+++ tr_ph/selection_scripts/select.cpp
@@ -0,0 +1,12 @@
+#include "TFile.h"
+#include "TROOT.h"
+#include "TTree.h"
+#include <iostream>
+
+int select(std::string macro_file, std::string macro_name, std::string root_filename, std::string output_fname) {
+    gROOT->LoadMacro(macro_file.c_str());
+    auto file = TFile::Open(root_filename.c_str());
+    gROOT->ProcessLine((macro_name + "a(tr_ph)").c_str());
+    gROOT->ProcessLine(("a.Loop(\"" + output_fname + "\")").c_str());
+    return 0;
+}

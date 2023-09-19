54f1f056baa04f9cda81b35d1b3e32148ab29b51
AliensWithdrawPeople <daniilivanov1606@gmail.com>
2023-09-19 22:55:54 +0700

    Fixed scripts for "multiple" selection.

--- tr_ph/selection_scripts/select.cpp
+++ tr_ph/selection_scripts/select.cpp
@@ -6,7 +6,7 @@
 int select(std::string macro_file, std::string macro_name, std::string root_filename, std::string output_fname) {
     gROOT->LoadMacro(macro_file.c_str());
     auto file = TFile::Open(root_filename.c_str());
-    gROOT->ProcessLine((macro_name + "a(tr_ph)").c_str());
+    gROOT->ProcessLine((macro_name + " a(tr_ph)").c_str());
     gROOT->ProcessLine(("a.Loop(\"" + output_fname + "\")").c_str());
     return 0;
 }
--- tr_ph/selection_scripts/select_driver.py
+++ tr_ph/selection_scripts/select_driver.py
@@ -3,8 +3,8 @@ import json
 from pathlib import PurePath
 import os
 
-prelim_selection_macro = 'prelim.cpp'
-prelim_selection_macro_name = 'prelim'
+prelim_selection_macro = 'prelim_coll.cpp'
+prelim_selection_macro_name = 'prelim_coll'
     
 def get_raw_file_root_url(scan: str, point: str)->str:
     return f'root://cmd//{scan}/{scan}_tr_ph_fc_e{point}_v9.root'
@@ -19,19 +19,26 @@ def execute_selection(scan_name: str, point: str, output_name: str):
                 \\{aux1 + prelim_selection_macro_name + aux2}\",\
                 \\{aux1 + get_raw_file_root_url(scan_name, point) + aux2}\", \
                 \\{aux1 + output_name + aux2}\")\""
-    res = sub.run(command, capture_output=True)
+    print(command)
+    res = sub.run(command, capture_output=True, shell=True)
     output = res.stderr if res.stderr else res.stdout
     print(res.stdout.decode()[118:])
 
-############# Main code below #############
+def run_EXP(scan_points_filename: os.PathLike):
+    if not os.path.isfile(scan_points_filename):
+        raise FileNotFoundError
+    with open(scan_points_filename) as file:
+        scans: dict = json.load(file)
 
-scan_points_filename = PurePath('tr_ph_names.json')
-if not os.path.isfile(scan_points_filename):
-    raise FileNotFoundError
-with open(scan_points_filename) as file:
-    scans: dict = json.load(file)
-    
-for scan_name, points in scans.items():
-    for point in points:
-        execute_selection(scan_name, point, f'prelim_{scan_name}_e{point}_coll.root')
-        
+    for scan_name, points in scans.items():
+        for point in points:
+            execute_selection(scan_name, point, f'prelim/{scan_name}_e{point}_coll_prelim.root')
+            
+      
+def run_MC(MC_info_file: os.PathLike):
+    # TODO
+    pass        
+
+############# Driver code #############
+scan_points_filename = PurePath('seasons.json')
+run_EXP(scan_points_filename)

3be03456899d5a46dc0025668b7a36366a802f7d
AliensWithdrawPeople <daniilivanov1606@gmail.com>
2023-10-21 20:20:42 +0700

    Added functionality  to work with MC.

--- tr_ph/selection_scripts/final/process_all_collinear.py
+++ tr_ph/selection_scripts/final/process_all_collinear.py
@@ -1,11 +1,76 @@
-from select_coll import Select_collinear, make_if_not_exists
+from enum import Enum, auto
 import pathlib
-from config import prelim_coll_root_files_folder, final_root_files_folder
+import re
+import json
+from multiprocessing.pool import Pool
+
+import sys
+sys.path.append('C:/work/Science/BINP/PbarP/tr_ph/')
+from config import PrelimRootFilesFolder_Coll, FinalRootFilesFolder
+from select_coll import Select_collinear, make_if_not_exists
 
 
-raw_files = [x for x in prelim_coll_root_files_folder.iterdir() if x.is_file() and x.suffix == '.root']
-for file in raw_files:
-    select_coll = Select_collinear(file)
-    energy_point_folder = pathlib.PurePath(final_root_files_folder, f'energy_{select_coll.energy_point}MeV')
+
+def select_exp(file: pathlib.Path, pattern: str, info: dict):
+    select_coll = Select_collinear(file, pattern)
+    energy_point_folder = pathlib.PurePath(FinalRootFilesFolder.exp.value, f'energy_{select_coll.energy_point}MeV')
     make_if_not_exists(energy_point_folder)
     select_coll.save(pathlib.PurePath(energy_point_folder, f'{select_coll.season}_coll.root'), True)
+    print('Entries after selection =', select_coll.selected_entries_num)
+    return '', {}
+
+def select_MC(file: pathlib.Path, MC_pattern: str, info: dict):
+    matched = re.fullmatch(MC_pattern, file.name)
+    if matched is None:
+        print(f"Couldn't match file {file.name}. It was skipped.")
+        return None
+    Ge = matched.group(3)
+    Gm = matched.group(4)
+    run_num = matched.group(5)
+    select_coll = Select_collinear(file, MC_pattern)
+    energy_point_folder = pathlib.PurePath(FinalRootFilesFolder.MC.value, f'energy_{select_coll.energy_point}MeV')
+    make_if_not_exists(energy_point_folder)
+    output_file_path = pathlib.PurePath(energy_point_folder, f'{select_coll.season}_coll_Ge{Ge}_Ge{Gm}_run000{run_num}.root')
+    is_saved = select_coll.save(output_file_path, True)
+    print(f'Entries after selection in run run000{run_num} =', select_coll.selected_entries_num)
+    try:
+        info[f'run000{run_num}']['selected_events'] = select_coll.selected_entries_num
+        info[f'run000{run_num}']['eff'] = select_coll.selected_entries_num / info[f'run000{run_num}']['events']
+        info[f'run000{run_num}']['processed_file_location'] = output_file_path.as_posix()
+    except KeyError as e:
+        print(f'Something is wrong with run000{run_num} in info file: {e}')
+    return f'run000{run_num}', info[f'run000{run_num}']
+
+class WorkingMode(Enum):
+    EXP = auto()     
+    MC = auto()    
+     
+if __name__ == '__main__':
+    # exp
+    raw_files = [x for x in PrelimRootFilesFolder_Coll.exp.value.iterdir() if x.is_file() and x.suffix == '.root']
+    exp_pattern = r'scan(\d+)_e([-+]?(?:\d*\.*\d+))_coll_prelim.root'
+    exp_info_filename = 'C:/work/Science/BINP/PbarP/tr_ph/exp_info.json'
+    # MC
+    raw_files = [x for x in PrelimRootFilesFolder_Coll.MC.value.iterdir() if x.is_file() and x.suffix == '.root']
+    MC_pattern = r'season(\d+)_e([-+]?(?:\d*\.*\d+))_Ge(\d+)_Gm(\d+)_coll_run000(\d+)_prelim.root'
+    MC_info_filename = 'C:/work/Science/BINP/PbarP/tr_ph/MC_info.json'
+    
+    # Set working mode
+    mode = WorkingMode.MC
+    
+    select = select_MC if mode is WorkingMode.MC else select_exp
+    info_filename = MC_info_filename if mode is WorkingMode.MC else exp_info_filename
+    with open(info_filename) as file:
+        info = json.load(file) 
+    
+    select_params = [(file, MC_pattern if mode is WorkingMode.MC else exp_pattern, info) for file in raw_files]
+    with Pool(4) as pool:
+        res = pool.starmap(select, select_params)
+    res = list(filter(lambda x: x is not None, res))
+    if all([x is not None for x in res]):    
+        res = dict(res) # type: ignore
+        for key, val in res.items():
+            info[key] = val
+    with open(info_filename, 'w') as file:
+        json.dump(info, file, indent=4) 
+        
--- tr_ph/selection_scripts/select_driver.py
+++ tr_ph/selection_scripts/select_driver.py
@@ -41,16 +41,44 @@ def get_selection_params_EXP(scan_points_filename: os.PathLike, event_type: Even
             output_name = f'prelim/{event_type.name}/{scan_name}_e{point}_{"coll" if event_type is Event_Type.collinear else "stars"}_prelim.root'
             selection_params.append((scan_name, point, output_name, event_type))  
     return selection_params 
-            
+
+def execute_selection_MC(filename: str, point: str, output_name: str, event_type: Event_Type):
+    prelim_selection_macro, prelim_selection_macro_name = event_type.value
+    prelim_selection_macro =  'C:/work/Science/BINP/PbarP/tr_ph/selection_scripts/prelim/' + prelim_selection_macro
+    command = f"root -l -q \"C:/work/Science/BINP/PbarP/tr_ph/selection_scripts/select.cpp(\\{aux1 + prelim_selection_macro + aux2}\",\
+                \\{aux1 + prelim_selection_macro_name + aux2}\",\
+                \\{aux1 + filename + aux2}\", \
+                \\{aux1 + output_name + aux2}\")\""
+    res = sub.run(command, capture_output=True, shell=True)
+    output = res.stderr if res.stderr else res.stdout
+    print(f'[{filename}/{point}MeV tr_ph v9 {event_type.name} exited with {res.returncode}]', flush=True)
+    print(output.decode()[93:], flush=True)            
       
-def get_selection_params_MC(MC_info_file: os.PathLike):
-    # TODO
-    pass        
+def get_selection_params_MC(scan_points_filename: os.PathLike, event_type: Event_Type, run_name_filter = None):
+    if not os.path.isfile(scan_points_filename):
+        raise FileNotFoundError
+    with open(scan_points_filename) as file:
+        MC_info: dict = json.load(file)
+
+    selection_params= []     
+    for run_name, info in MC_info.items():
+        if run_name_filter is not None and not run_name_filter(run_name):
+            continue
+        season_num = info['season'][-4:] if info['season'] is not None else 0
+        output_name = f'C:/work/Science/BINP/PbarP/tr_ph/root_files/MC/prelim/{event_type.name}/season{season_num}_e{info["E_beam"]}_Ge{info["Ge^2"]}_Gm{info["Gm^2"]}_{"coll" if event_type is Event_Type.collinear else "stars"}_{run_name}_prelim.root'
+        selection_params.append((f'C:/work/Science/BINP/PbarP/{info["location"]}', info["E_beam"], output_name, event_type))  
+    return selection_params    
 
 ############# Driver code #############
 if __name__ == '__main__':
-    scan_points_filename = PurePath('seasons.json')
-    # get_selection_params_MC(scan_points_filename, Event_Type.collinear)
-    params = get_selection_params_EXP(scan_points_filename, Event_Type.annihilation)
+    # exp_scan_points_filename = PurePath('tr_ph/seasons.json')
+    # params = get_selection_params_EXP(exp_scan_points_filename, Event_Type.collinear)
+    # with Pool(4) as pool:
+    #     pool.starmap(execute_selection, params)  
+        
+    mc_scan_points_filename = PurePath('tr_ph/MC_info.json')
+    params = get_selection_params_MC(mc_scan_points_filename, Event_Type.collinear, 
+                                     lambda run_name: str(run_name).startswith("run0001") or str(run_name).startswith("run0002"))
     with Pool(4) as pool:
-        pool.starmap(execute_selection, params)  
+        pool.starmap(execute_selection_MC, params) 
+

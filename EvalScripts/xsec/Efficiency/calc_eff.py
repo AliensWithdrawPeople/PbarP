47b85d478213a23a4f4b096eecd732584e3794f7
AliensWithdrawPeople <daniilivanov1606@gmail.com>
2023-12-14 20:34:24 +0700

    Added efficiency calc for stars.

--- EvalScripts/xsec/Efficiency/calc_eff.py
+++ EvalScripts/xsec/Efficiency/calc_eff.py
@@ -4,23 +4,29 @@ import pathlib
 
 import sys
 sys.path.append('C:/work/Science/BINP/PbarP')
-from tr_ph.config import MC_info_path, collinear_results_data, GeGm_Fit_Result_json, templates, root_folder
+from tr_ph.config import MC_info_path, collinear_results_data, stars_results_data, GeGm_Fit_Result_json, templates, root_folder
+
+type = "coll"
+type = "stars"
+results_data = collinear_results_data if type == "coll" else stars_results_data
+eff_key_name = 'eff' if type == "coll" else 'eff_stars'
 
 GeGm = json.loads(GeGm_Fit_Result_json.read_text())
 MC_info = json.loads(MC_info_path.read_text())
-vis_xsec_data = json.loads(collinear_results_data.read_text())
+vis_xsec_data = json.loads(results_data.read_text())
 
-def eval_eff(elabel: str)->tuple[float, float]:
+def load_MC_info(elabel: str):
     if elabel not in GeGm.keys():
         raise KeyError(f'No GeGm for elabel = {elabel}')
     
     if GeGm[elabel]['status'] is not True:
         raise ValueError(f'GeGm fit status is {GeGm[elabel]["status"]}') 
     info = GeGm[elabel]
-    
     ge, ge_error = info['Ge2']
     gm, gm_error = info['Gm2']
-    
+    return ge, ge_error, gm, gm_error
+
+def eval_eff(elabel: str, ge: float, ge_error: float, gm: float, gm_error: float)->tuple[float, float]:
     ge_frac = ge / (ge + gm)
     gm_frac = gm / (ge + gm)
     
@@ -32,8 +38,11 @@ def eval_eff(elabel: str)->tuple[float, float]:
     ge_mc_num = 0 if mc[0][1]['Ge^2'] != 0 else 1
     gm_mc_num = 1 if mc[0][1]['Ge^2'] != 0 else 0
     
-    eff_ge = mc[ge_mc_num][1]['eff']
-    eff_gm = mc[gm_mc_num][1]['eff']
+    if int(elabel[:3]) < 960:
+        ge_frac, gm_frac = 0.5, 0.5
+    
+    eff_ge = mc[ge_mc_num][1][eff_key_name]
+    eff_gm = mc[gm_mc_num][1][eff_key_name]
     eff = eff_ge * ge_frac + eff_gm * gm_frac
     eff_error = (((eff_ge + eff_gm) * gm / (ge + gm)**2)**2 * ge_error**2 + 
                  ((eff_ge + eff_gm) * ge / (ge + gm)**2)**2 * gm_error**2 + 
@@ -44,12 +53,22 @@ def eval_eff(elabel: str)->tuple[float, float]:
 
 res = []
 for elabel, xsec in vis_xsec_data.items():
-    if xsec['nominal_energy'] < 950 or xsec['season'][-4:] == '2017':
+    # if xsec['nominal_energy'] < 950 or xsec['season'][-4:] == '2017':
+    if xsec['nominal_energy'] > 950:
         continue
-    
-    eff, eff_err = eval_eff(elabel)
-    xsection, xsection_err = xsec["cross section"], xsec["cross section error"]
-    res.append((xsec['season'], xsec['energy'], round(xsec["cross section"] / eff, 4), round(((xsection_err / eff)**2 + (xsection * eff_err / eff**2)**2)**0.5, 4)))
+    ge, ge_error, gm, gm_error = 1, 0.1, 1, 0.1
+    try:
+        ge, ge_error, gm, gm_error = load_MC_info(elabel)
+    except Exception as e:
+        # print(elabel, ":", e)
+        pass
+        
+    try:    
+        eff, eff_err = eval_eff(elabel, ge, ge_error, gm, gm_error)
+        xsection, xsection_err = xsec["cross section"], xsec["cross section error"]
+        res.append((xsec['season'], xsec['energy'], round(xsec["cross section"] / eff, 4), round(((xsection_err / eff)**2 + (xsection * eff_err / eff**2)**2)**0.5, 4)))
+    except KeyError as e:
+        print(elabel, ":", e)
 
 res.sort(key=lambda x: x[0])
 
@@ -76,7 +95,8 @@ def render(data, template_filename, output_filename):
     with open(output_filename, "w") as file:
         file.write(rendered_content)
 
-template_filename = pathlib.Path(templates, 'xsec_coll.cpp.jinja')
-output_filename = pathlib.Path(root_folder, 'graph_drawing_scripts/xsec_coll.cpp')
+
+template_filename = pathlib.Path(templates, f'xsec_{type}.cpp.jinja')
+output_filename = pathlib.Path(root_folder, f'graph_drawing_scripts/xsec_{type}.cpp')
 
 render(season_list, template_filename, output_filename)

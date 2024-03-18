3f0521379e46733461f620a33f911b41b2d9788d
AliensWithdrawPeople <daniilivanov1606@gmail.com>
2024-03-18 20:32:45 +0700

    Added processed_file_location_stars and eff_stars for FTFP_BERT_HP

--- tr_ph/selection_scripts/final/process_all_stars.py
+++ tr_ph/selection_scripts/final/process_all_stars.py
@@ -34,27 +34,30 @@ def select_exp(file: pathlib.Path, pattern: str, info: dict):
     print('Entries after selection =', select_stars.get_selected_entries_num())
     return
 
-def select_MC(file: pathlib.Path, MC_pattern: str, info: dict):
-    matched = re.fullmatch(MC_pattern, file.name)
+def select_MC(file: pathlib.Path, MC_pattern: str, info: dict, json_key: str | None = None):
+    matched = re.search(MC_pattern, file.name)
     if matched is None:
         print(f"Couldn't match file {file.name}. It was skipped.")
         return None
     Ge = matched.group(3)
     Gm = matched.group(4)
-    run_num = matched.group(5)
+    run_num = 0 # matched.group(5)
     select_stars = Select_stars(file, MC_pattern)
     energy_point_folder = pathlib.PurePath(FinalRootFilesFolder.MC.value, f'energy_{select_stars.energy_point}MeV')
     make_if_not_exists(energy_point_folder)
-    output_file_path = pathlib.PurePath(energy_point_folder, f'{select_stars.season}_stars_Ge{Ge}_Ge{Gm}_run000{run_num}.root')
+    # output_file_path = pathlib.PurePath(energy_point_folder, f'{select_stars.season}_stars_Ge{Ge}_Ge{Gm}_run000{run_num}.root')
+    output_file_path = pathlib.PurePath(energy_point_folder, f'{select_stars.season}_stars_Ge{Ge}_Ge{Gm}_FTFP_BERT_HP.root')
     is_saved = select_stars.save(output_file_path, True)
-    print(f'Entries after selection in run run000{run_num} =', select_stars.get_selected_entries_num())
+    print(f'Entries after selection in run {json_key} =', select_stars.get_selected_entries_num())
+    print(f'output_file_path = {output_file_path}')
+
     try:
-        info[f'run000{run_num}']['selected_events_stars'] = select_stars.get_selected_entries_num()
-        info[f'run000{run_num}']['eff_stars'] = select_stars.get_selected_entries_num() / info[f'run000{run_num}']['events']
-        info[f'run000{run_num}']['processed_file_location_stars'] = output_file_path.as_posix()
+        info[json_key]['selected_events_stars'] = select_stars.get_selected_entries_num()
+        info[json_key]['eff_stars'] = select_stars.get_selected_entries_num() / info[json_key]['events']
+        info[json_key]['processed_file_location_stars'] = output_file_path.as_posix()
     except KeyError as e:
-        print(f'Something is wrong with run000{run_num} in info file: {e}')
-    return f'run000{run_num}', info[f'run000{run_num}']
+        print(f'Something is wrong with {json_key} in info file: {e}')
+    return json_key, info[json_key]
 
 
 
@@ -70,23 +73,41 @@ if __name__ == '__main__':
     # MC
     raw_files_MC = [x for x in PrelimRootFilesFolder_Stars.MC.value.iterdir() if x.is_file() and x.suffix == '.root']
     MC_pattern = r'season(\d+)_e([-+]?(?:\d*\.*\d+))_Ge(\d+)_Gm(\d+)_stars_run000(\d+)_prelim.root'
+    MC_pattern = r'season(\d+)_e([-+]?(?:\d*\.*\d+))_Ge(\d+)_Gm(\d+)'
     MC_info_filename = 'C:/work/Science/BINP/PbarP/tr_ph/MC_info.json'
     
     # Set working mode
-    mode = WorkingMode.EXP
+    mode = WorkingMode.MC
     
     select = select_MC if mode is WorkingMode.MC else select_exp
     raw_files = raw_files_MC if mode is WorkingMode.MC else raw_files_exp
     info_filename = MC_info_filename if mode is WorkingMode.MC else exp_info_filename
+    def get_json_key(filename: str):
+        pattern_string =  r'season(\d+)_e([-+]?(?:\d*\.*\d+))_Ge(\d+)_Gm(\d+)'
+        matched = re.search(pattern_string, filename)
+        if matched is None:
+            print('sear')
+            return None
+        season = matched.group(1)
+        energy = matched.group(2)
+        Ge = matched.group(3)
+        Gm = matched.group(4)
+
+        for run, run_data in info.items():
+            if int(run_data['season'][-4:]) == int(season) and run_data["Ge^2"] == int(Ge) and run_data["Gm^2"] == int(Gm) and \
+                abs(run_data['E_beam'] - float(energy)) < 0.1 and run_data['Physics list'] == 'FTFP_BERT_HP':
+                return run
+        return None
+    
     with open(info_filename) as file:
         info = json.load(file) 
-    raw_files = [pathlib.PurePath('C:/work/Science/BINP/PbarP/tr_ph/root_files/MC/prelim/stars/season2022_e941.995_Ge1_Gm0_stars_run000146_prelim.root')]
-    select_params = [(file, MC_pattern if mode is WorkingMode.MC else exp_pattern, info) for file in raw_files]
+
+    select_params = [(file, MC_pattern if mode is WorkingMode.MC else exp_pattern, info, get_json_key(file.name)) for file in raw_files]
     
     def progress(res):
         print(f"Done:{len(res)} out of {len(select_params)}")
         
-    with Pool(4) as pool:
+    with Pool(6) as pool:
         res = pool.starmap(select, select_params)
     res = list(filter(lambda x: x is not None, res))
     if all([x is not None for x in res]):    
